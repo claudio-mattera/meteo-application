27c4486f634ca7ccf8c6af721463bb9de5a71b20
Claudio Mattera <claudio@mattera.it>
2015-03-07 12:23:33 +0100

    Estracting dates from db in localtime.

--- charts/make_csv_files.sh
+++ charts/make_csv_files.sh
@@ -4,8 +4,8 @@ DESTDIR="/var/www"
 
 > $DESTDIR/pressure.csv
 echo "date,internal_temperature,temperature,pressure" >> $DESTDIR/pressure.csv
-sqlite3 /var/lib/meteo/data.db "SELECT datetime(dateTime),internalTemperature,pressureTemperature,pressurePressure FROM MeteoData" | sed s/\|/,/g >> $DESTDIR/pressure.csv
+sqlite3 /var/lib/meteo/data.db "SELECT datetime(dateTime, 'localtime'),internalTemperature,pressureTemperature,pressurePressure FROM MeteoData" | sed s/\|/,/g >> $DESTDIR/pressure.csv
 
 > $DESTDIR/light.csv
 echo "date,internal_temperature,temperature,light" >> $DESTDIR/light.csv
-sqlite3 /var/lib/meteo/data.db "SELECT datetime(dateTime),internalTemperature,pressureTemperature,lightVisible FROM MeteoData" | sed s/\|/,/g >> $DESTDIR/light.csv
+sqlite3 /var/lib/meteo/data.db "SELECT datetime(dateTime, 'localtime'),internalTemperature,pressureTemperature,lightVisible FROM MeteoData" | sed s/\|/,/g >> $DESTDIR/light.csv
