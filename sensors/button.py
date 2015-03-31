559471334db04f1c604bfa865384ba922fbf8ac1
Claudio Mattera <claudio@mattera.it>
2015-03-31 21:43:07 +0200

    Specifying shutdown pin from command line.

--- sensors/button.py
+++ sensors/button.py
@@ -3,10 +3,11 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import logging
+import argparse
 
 keep = True
 
-def callback(pin):
+def shutdown_callback(pin):
     logging.info("Callback called on pin %d" % pin)
     global keep
     keep = False
@@ -17,14 +18,32 @@ def main_loop():
     while keep:
         sleep(1)
 
+def parse_command_line():
+    parser = argparse.ArgumentParser(
+        description='Handle button press')
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Increase logging')
+    parser.add_argument(
+        '-s', '--shutdown-pin',
+        type=int,
+        default=21,
+        help='The pin corresponding to shutdown button')
+    return parser.parse_args()
+
 def main():
-    pin = 21
+    arguments = parse_command_line()
+    if arguments.verbose:
+        logging.basicConfig(level=logging.INFO)
+    
+    logging.info("Waiting for shutdown event on pin %d" % arguments.shutdown_pin)
 
     try:
         logging.info('Initializing GPIO')
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(pin, GPIO.RISING, callback=callback, bouncetime=3000)
+        GPIO.setup(arguments.shutdown_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.add_event_detect(arguments.shutdown_pin, GPIO.RISING, callback=shutdown_callback, bouncetime=3000)
         main_loop()
     finally:
         logging.info('Cleaning up GPIO')
