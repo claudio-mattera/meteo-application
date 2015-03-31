16488da36374394824b273ce52e04095a6d35bdf
Claudio Mattera <claudio@mattera.it>
2015-03-31 21:29:09 +0200

    Added logging to button detector script.

--- sensors/button.py
+++ sensors/button.py
@@ -7,11 +7,12 @@ import logging
 keep = True
 
 def pin_callback(pin):
-    print("Callback called on pin %d" % pin)
+    logging.info("Callback called on pin %d" % pin)
     global keep
     keep = False
 
 def main_loop():
+    logging.info('Entering main loop')
     global keep
     while keep:
         time.sleep(1)
@@ -20,13 +21,16 @@ def main():
     pin = 21
 
     try:
+        logging.info('Initializing GPIO')
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         GPIO.add_event_detect(pin, GPIO.RISING, callback=pin_callback, bouncetime=3000)
         main_loop()
     finally:
+        logging.info('Cleaning up GPIO')
         GPIO.cleanup()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
