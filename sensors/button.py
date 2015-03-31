f72f7f8ad1de8008ba24352e740503561590c7fd
Claudio Mattera <claudio@mattera.it>
2015-03-31 21:34:47 +0200

    Cleanup.

--- sensors/button.py
+++ sensors/button.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 import logging
 
 keep = True
 
-def pin_callback(pin):
+def callback(pin):
     logging.info("Callback called on pin %d" % pin)
     global keep
     keep = False
@@ -15,7 +15,7 @@ def main_loop():
     logging.info('Entering main loop')
     global keep
     while keep:
-        time.sleep(1)
+        sleep(1)
 
 def main():
     pin = 21
@@ -24,7 +24,7 @@ def main():
         logging.info('Initializing GPIO')
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(pin, GPIO.RISING, callback=pin_callback, bouncetime=3000)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=callback, bouncetime=3000)
         main_loop()
     finally:
         logging.info('Cleaning up GPIO')
