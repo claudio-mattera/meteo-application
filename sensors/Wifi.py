75a6bc3fe33d0d7b5c0f6d11b99ed8455ebbdd5d
Claudio Mattera <claudio@mattera.it>
2016-05-16 21:38:06 +0200

    Fixed wifi presence sensor.

--- sensors/Wifi.py
+++ sensors/Wifi.py
@@ -4,24 +4,21 @@ import subprocess
 import re
 
 
+PATTERN = re.compile(
+    '(?P<ip>\\d+\\.\\d+\\.\\d+\\.\\d+)\\s+'
+    '(?P<mac>\\w+:\\w+:\\w+:\\w+:\\w+:\\w+)\\s*\n')
+
+
 class Wifi:
 
     def __init__(self, known_devices):
-        if isinstance(known_devices, dict):
-            self.known_devices = known_devices
-        elif isinstance(known_devices, str):
-            self.known_devices = self._load_dict_from_file(known_devices)
-        else:
-            raise RuntimeError('"known_devices" must be a dict or a filename')
-        self.pattern = re.compile(
-            '(?P<ip>\\d+\\.\\d+\\.\\d+\\.\\d+)\\s+'
-            '(?P<mac>\\w+:\\w+:\\w+:\\w+:\\w+:\\w+)\\s*\n')
+        self.known_devices = known_devices['known_devices']
 
     def read_active_nics(self):
         cmd = ['arp-scan', '--interface=wlan0', '--localnet', '--quiet']
         outcome = subprocess.check_output(cmd)
         string = outcome.decode('utf-8')
-        matches = self.pattern.findall(string)
+        matches = PATTERN.findall(string)
         return [mac.lower() for (ip, mac) in matches]
 
     def read_active_devices(self):
@@ -39,11 +36,6 @@ class Wifi:
         active_devices = self.read_active_devices()
         return len(active_devices - set([0]))
 
-    def _load_dict_from_file(self, filename):
-        import ast
-        with open(filename, 'r') as file:
-            content = file.read()
-            return ast.literal_eval(content)
 
 if __name__ == '__main__':
     import sys
